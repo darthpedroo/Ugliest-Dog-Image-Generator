fe77dee8975cb51eee58f7b35dc5fdd522410f26
darthpedroo <79163424+darthpedroo@users.noreply.github.com>
2024-07-01 12:48:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Ugliest-Dog-Imae-Generator/',
+  base: '/Ugliest-Dog-Image-Generator/',
   plugins: [
     vue(),
   ],
